c0e349ee8a2497ec0a4804c2db5323d4fd771aa4
Emanuel Montenegro <emanuel.montenegro@mail.uft.edu.br>
2023-10-11 22:02:36 -0300

    .

--- src/main/java/tech/ada/ecommerce/controller/HelloWorldController.java
+++ src/main/java/tech/ada/ecommerce/controller/HelloWorldController.java
@@ -1,17 +1,37 @@
 package tech.ada.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.ada.ecommerce.model.Cliente;
+import tech.ada.ecommerce.service.ClienteService;
 
-@RestController //define como controller
-@RequestMapping("/api/v1") //define como o endpoint principal
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class HelloWorldController {
-    //o controller controla meus endpoints
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+    private final ClienteService clienteService;
+
+    public HelloWorldController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
 
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
         return "Olá mundo!!";
+    }
+
+    @GetMapping("/clientes/{nome}")
+    public List<Cliente> buscarClientes(@PathVariable("nome") String nome) {
+        return clienteService.buscarPorNomeCompleto(nome);
     }
 }
